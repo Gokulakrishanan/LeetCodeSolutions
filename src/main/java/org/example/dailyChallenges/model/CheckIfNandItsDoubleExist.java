6ec250ea6de47de3ad1dc3859500a760f9fc1378
gokul <gokulakrishanan54@gmail.com>
2024-12-03 01:22:25 -0500

    LeetCode Dec 03 Solution

---
+++ src/main/java/org/example/dailyChallenges/model/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,19 @@
+package org.example.dailyChallenges.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CheckIfNandItsDoubleExist {
+
+    public boolean checkIfExist(int[] nums){
+        Set<Integer> resultSet = new HashSet<>();
+
+        for (int num : nums) {
+            if(resultSet.contains(2*num) || (num%2==0 && resultSet.contains(num/2) )){
+                return true;
+            }
+            resultSet.add(num);
+        }
+        return false;
+    }
+}
