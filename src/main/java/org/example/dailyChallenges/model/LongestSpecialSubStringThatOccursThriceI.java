49ab9f65cc00aa5059636742a845fe1cd58fd832
gokul <gokulakrishanan54@gmail.com>
2024-12-10 10:33:04 -0500

    LeetCode Dec 9 & 10 Solution

---
+++ src/main/java/org/example/dailyChallenges/model/LongestSpecialSubStringThatOccursThriceI.java
@@ -0,0 +1,31 @@
+package org.example.dailyChallenges.model;
+
+import java.util.HashMap;
+
+public class LongestSpecialSubStringThatOccursThriceI {
+    public int maximumLength(String input){
+        HashMap<String,Integer> map = new HashMap<>();
+
+        int n = input.length();
+        for (int i = 0; i <n ; i++) {
+            int j =i;
+            do {
+                String substring = input.substring(i, j + 1);
+                map.put(substring,map.getOrDefault(substring,0)+1);
+                if (j<n-1 && input.charAt(j)==input.charAt(j+1)){
+                    j++;
+                }else{
+                    break;
+                }
+            }while(j<n);
+        }
+        String subStringMax = "";
+
+        for (String s : map.keySet()) {
+            if (map.get(s)>=3 && s.length()>subStringMax.length()){
+                subStringMax = s;
+            }
+        }
+        return subStringMax ==""? -1 : subStringMax.length();
+    }
+}
