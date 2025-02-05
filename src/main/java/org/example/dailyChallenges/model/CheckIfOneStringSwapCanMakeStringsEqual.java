145e2b566dc1fd50c61ccb801aed260ebfc8b034
gokul <gokulakrishanan54@gmail.com>
2025-02-04 20:52:31 -0500

    LeetCode Solutions

---
+++ src/main/java/org/example/dailyChallenges/model/CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,45 @@
+package org.example.dailyChallenges.model;
+
+/*You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different) and swap the characters at these indices.
+
+Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings. Otherwise, return false.
+ Example 1:
+Input: s1 = "bank", s2 = "kanb"
+Output: true
+Explanation: For example, swap the first character with the last character of s2 to make "bank".
+Example 2:
+Input: s1 = "attack", s2 = "defend"
+Output: false
+Explanation: It is impossible to make them equal with one string swap.
+Example 3:
+Input: s1 = "kelb", s2 = "kelb"
+Output: true
+Explanation: The two strings are already equal, so no string swap operation is required.
+Constraints:
+1 <= s1.length, s2.length <= 100
+s1.length == s2.length
+s1 and s2 consist of only lowercase English letters.
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckIfOneStringSwapCanMakeStringsEqual {
+
+    public boolean areAlmostEqual(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                list.add(i);
+            }
+            if (list.size() > 2) {
+                return false;
+            }
+        }
+        return list.size() == 2 && s1.charAt(list.get(0)) == s2.charAt(list.get(1)) && s1.charAt(list.get(1)) == s2.charAt(list.get(0));
+    }
+
+}
